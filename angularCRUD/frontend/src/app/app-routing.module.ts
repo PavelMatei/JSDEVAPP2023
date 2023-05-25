f72d49b60ca96b46f8943ebea97da5f984a52f78
Frontend JavaScript <frontendjavascript@C3-22.local>
2023-05-25 20:54:42 +0300

    curs2

---
+++ angularCRUD/frontend/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+const routes: Routes = [
+  { path: 'prima-pagina', component: HomeComponent },
+  { path: 'admin' , component: AdminComponent },
+  { path: '**', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ curs1/angularDemo/src/app/alerta.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertaService {
+
+  constructor() { }
+  afiseazaMesaj(mesaj:string){
+    alert(mesaj);
+  }
+}
