28a618a7ee80d8414af8bd4a517c1a2d9162dd1c
Frontend JavaScript <frontendjavascript@C3-22.local>
2023-05-30 21:13:03 +0300

    curs3

--- angularCRUD/frontend/src/app/admin/admin.component.ts
+++ angularCRUD/frontend/src/app/admin/admin.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from '../api.service';
+import { User } from '../models/user';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
+  users:any[]=[];
+  selectedUser:User={id:0,nume:'',prenume:'',email:'',datanastere:new Date(),telefon:''}
+  constructor(private apiService:ApiService) { }
 
   ngOnInit(): void {
   }
-
+  createOrUpdateUser( form:{value:User;}){
+    if(this.selectedUser && this.selectedUser.id){
+      // this.apiService.updateUser(this.selectedUser.id,form.value).subscribe((user:User) =>{
+      //   console.log('User updated',user);
+      // });
+    }
+    else{
+      this.apiService.createUser(form.value).subscribe((user:User)=>{
+        // this.readUsers();
+        console.log('User created',user);
+      })
+    }
+  }
 }
